417b500c3f6c35d146fd54c150a1061851e76106
Michael McGarry <mike.mcgarry@finbourne.com>
2019-04-09 08:53:41 +0100

    Added test extension to filenames of tests

---
+++ sdk/lib/test/tutorials/metadata.test.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Require the LUSID client and libraries
+var clientAuthentication = require('./apiClientAuthentication.js');
+var Client = require('./apiClientInitialisation').Client;
+var client = new Client();
+var clientBuilder = clientAuthentication.lusidApiClientBuilder;
+clientBuilder(client).then(function (client) {
+    client.api.applicationMetadata.getLusidVersions().then(function (result) {
+        console.log(result);
+    });
+});
