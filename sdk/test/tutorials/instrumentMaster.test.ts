f0ec0efb7bf984c83d86fcb2f5d2658edce3f80f
Mike McGarry <mikemcgarry@live.com.au>
2019-09-04 09:48:31 +0100

    DEV-4494 Updated the use of upserting instrument properties@

--- sdk/test/tutorials/instrumentMaster.test.ts
+++ sdk/test/tutorials/instrumentMaster.test.ts
@@ -48,10 +48,10 @@ function loadFromCsv(filePath: string): Promise<any[]> {
   return new Promise((resolve, reject) => {
     // Use the csvtojson module to import a CSV file
     csv()
-    .fromFile(filePath)
-    // Produces an array of objects, one for each row (instrument) of the CSV
-    .then((instruments: any[]) => resolve(instruments))
-    .catch((err) => reject(err))
+        .fromFile(filePath)
+        // Produces an array of objects, one for each row (instrument) of the CSV
+        .then((instruments: any[]) => resolve(instruments))
+        .catch((err) => reject(err))
   })
 }
 
@@ -70,122 +70,122 @@ function loadFromJson(filePath: string): Promise<any[]> {
 }
 
 enum FileType {
-    Json = "Json",
-    Csv = "Csv",
+  Json = "Json",
+  Csv = "Csv",
 }
 
 function upsertInstrumentsFromFile(
-  filePath: string,
-  fileType: FileType
-  ): Promise<UpsertInstrumentsResponse> {
+    filePath: string,
+    fileType: FileType
+): Promise<UpsertInstrumentsResponse> {
 
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
 
-      if (fileType == FileType.Json) {
-        var loadFunction = loadFromJson(filePath)
-      } else {
-        var loadFunction = loadFromCsv(filePath)
-      }
+    if (fileType == FileType.Json) {
+      var loadFunction = loadFromJson(filePath)
+    } else {
+      var loadFunction = loadFromCsv(filePath)
+    }
 
-      loadFunction.then((instruments: any[]) => {
-        // Use a reduce function to convert each instrument object into a LUSID model
-        return instruments.reduce((map: {[key: string]: InstrumentDefinition}, instrument: any) => {
-          // Call your conversion function defined earlier to convert each instrument
-          map[instrument.instrument_name] = buildUpsertInstrumentRequest(instrument)
-          return map
-        }, {})
-      })
-      .then((instrumentDefinitions: {[key: string]: InstrumentDefinition}) => {
-        // Use your client to call upsert instruments
-        return client.api.instruments.upsertInstruments(instrumentDefinitions)
-        })
-      .then((res: any) => resolve(res.body))
-      .catch((err: any) => reject(err))
+    loadFunction.then((instruments: any[]) => {
+      // Use a reduce function to convert each instrument object into a LUSID model
+      return instruments.reduce((map: {[key: string]: InstrumentDefinition}, instrument: any) => {
+        // Call your conversion function defined earlier to convert each instrument
+        map[instrument.instrument_name] = buildUpsertInstrumentRequest(instrument)
+        return map
+      }, {})
     })
-  }
+        .then((instrumentDefinitions: {[key: string]: InstrumentDefinition}) => {
+          // Use your client to call upsert instruments
+          return client.api.instruments.upsertInstruments(instrumentDefinitions)
+        })
+        .then((res: any) => resolve(res.body))
+        .catch((err: any) => reject(err))
+  })
+}
 
 
 // Use your client to call LUSID and create a new property
 function createProperty(
-  propertyDefintion: CreatePropertyDefinitionRequest):
-  Promise<PropertyDefinition> {
-    // Return a promise
-    return new Promise((resolve, reject) => {
-      // Use your client to call create property definition
-      client.api.propertyDefinitions.createPropertyDefinition(propertyDefintion)
-      .then((res: any) => resolve(res.body))
-      .catch((err: any) => reject(err))
-    })
-  }
+    propertyDefintion: CreatePropertyDefinitionRequest):
+    Promise<PropertyDefinition> {
+  // Return a promise
+  return new Promise((resolve, reject) => {
+    // Use your client to call create property definition
+    client.api.propertyDefinitions.createPropertyDefinition(propertyDefintion)
+        .then((res: any) => resolve(res.body))
+        .catch((err: any) => reject(err))
+  })
+}
 
 // Gets tne Lusid Instrument Id for a list of instruments
 function getLuidForInstruments(
-  identifierType: string,
-  identifierValues: string[]
-  ): Promise<{[key: string]: string}> {
-    // Return a promise
-    return new Promise((resolve, reject) => {
-      // Using your client call LUSID to get the instrument definitions
-      client.api.instruments.getInstruments(identifierType, identifierValues)
-      .then((res: any) => res.body)
-      .then((res: GetInstrumentsResponse) => {
-        // Pick off the Lusid Instrument ID for each definition and discard the rest
-        return Object.keys(res.values).reduce(
-          (map: {[key: string]: string}, instrumentIdentifier: string) => {
-            map[instrumentIdentifier] = res.values[instrumentIdentifier].lusidInstrumentId
-            return map
-          }, {})
-      })
-      .then((lusidInstrumentIdMapping: {[key: string]: string}) => {
-        resolve(lusidInstrumentIdMapping)
-      })
-      .catch((err: any) => reject(err))
-    })
-  }
+    identifierType: string,
+    identifierValues: string[]
+): Promise<{[key: string]: string}> {
+  // Return a promise
+  return new Promise((resolve, reject) => {
+    // Using your client call LUSID to get the instrument definitions
+    client.api.instruments.getInstruments(identifierType, identifierValues)
+        .then((res: any) => res.body)
+        .then((res: GetInstrumentsResponse) => {
+          // Pick off the Lusid Instrument ID for each definition and discard the rest
+          return Object.keys(res.values).reduce(
+              (map: {[key: string]: string}, instrumentIdentifier: string) => {
+                map[instrumentIdentifier] = res.values[instrumentIdentifier].lusidInstrumentId
+                return map
+              }, {})
+        })
+        .then((lusidInstrumentIdMapping: {[key: string]: string}) => {
+          resolve(lusidInstrumentIdMapping)
+        })
+        .catch((err: any) => reject(err))
+  })
+}
 
 // Get the Lusid Instrument Id for our instruments
 function addLusidInstrumentIdsFromFile(
-  filePath: string,
-  fileType: FileType
-  ): Promise<any[]> {
+    filePath: string,
+    fileType: FileType
+): Promise<any[]> {
 
-    return new Promise((resolve, reject) => {
-      if (fileType == FileType.Json) {
-        var loadFunction = loadFromJson(filePath)
-      } else {
-        var loadFunction = loadFromCsv(filePath)
-      }
-      // Get your instruments from a the file
-      loadFunction
-      .then((instruments: any[]) => {
-        // Get the figi for each instrument
-        return [
-          instruments,
-          instruments.reduce((figis: string[], instrument: any) => {
-            figis.push(instrument.figi)
-            return figis
-          }, [])
-        ]
-      })
-      .then(([instruments, figis]) => {
-        // Using the Figi retrieve each instruments LusidInstrumentId
-        return getLuidForInstruments('Figi', figis)
-        .then((lusidInstrumentIdMapping) => {
+  return new Promise((resolve, reject) => {
+    if (fileType == FileType.Json) {
+      var loadFunction = loadFromJson(filePath)
+    } else {
+      var loadFunction = loadFromCsv(filePath)
+    }
+    // Get your instruments from a the file
+    loadFunction
+        .then((instruments: any[]) => {
+          // Get the figi for each instrument
           return [
             instruments,
-            lusidInstrumentIdMapping
+            instruments.reduce((figis: string[], instrument: any) => {
+              figis.push(instrument.figi)
+              return figis
+            }, [])
           ]
         })
-      })
-      .then(([instruments, lusidInstrumentIdMapping]) => {
-        return instruments.map((instrument: any) => {
-          instrument.lusidinstrumentid = lusidInstrumentIdMapping[instrument.figi]
-          return instrument
+        .then(([instruments, figis]) => {
+          // Using the Figi retrieve each instruments LusidInstrumentId
+          return getLuidForInstruments('Figi', figis)
+              .then((lusidInstrumentIdMapping) => {
+                return [
+                  instruments,
+                  lusidInstrumentIdMapping
+                ]
+              })
         })
-      })
-      .then((instruments: any[]) => resolve(instruments))
-      .catch((err: any) => reject(err))
-    })
+        .then(([instruments, lusidInstrumentIdMapping]) => {
+          return instruments.map((instrument: any) => {
+            instrument.lusidinstrumentid = lusidInstrumentIdMapping[instrument.figi]
+            return instrument
+          })
+        })
+        .then((instruments: any[]) => resolve(instruments))
+        .catch((err: any) => reject(err))
+  })
 }
 
 function buildInstrumentProperty(key: string, value: string): Property {
@@ -198,39 +198,40 @@ function buildInstrumentProperty(key: string, value: string): Property {
 }
 
 function buildUpsertInstrumentPropertiesRequest(
-  key: string,
-  property: string,
-  instruments: any[]):
-  UpsertInstrumentPropertyRequest[] {
-    return instruments.reduce((list: UpsertInstrumentPropertyRequest[], instrument: any) => {
-      let instrumentPropertyRequest = new UpsertInstrumentPropertyRequest()
-      instrumentPropertyRequest.lusidInstrumentId = instrument.lusidinstrumentid
-      instrumentPropertyRequest.properties = [
-        buildInstrumentProperty(
+    key: string,
+    property: string,
+    instruments: any[]):
+    UpsertInstrumentPropertyRequest[] {
+  return instruments.reduce((list: UpsertInstrumentPropertyRequest[], instrument: any) => {
+    let instrumentPropertyRequest = new UpsertInstrumentPropertyRequest()
+    instrumentPropertyRequest.identifierType = "LusidInstrumentId"
+    instrumentPropertyRequest.identifier = instrument.lusidinstrumentid
+    instrumentPropertyRequest.properties = [
+      buildInstrumentProperty(
           key,
           instrument[property]
-        )
-      ]
-      list.push(instrumentPropertyRequest)
-      return list
-    }, [])
-  }
+      )
+    ]
+    list.push(instrumentPropertyRequest)
+    return list
+  }, [])
+}
 
 function upsertInstrumentProperties(
-  key: string,
-  property: string,
-  instruments: any[]):
-  Promise<UpsertInstrumentPropertiesResponse> {
-    return new Promise((resolve, reject) => {
-      client.api.instruments.upsertInstrumentsProperties(
+    key: string,
+    property: string,
+    instruments: any[]):
+    Promise<UpsertInstrumentPropertiesResponse> {
+  return new Promise((resolve, reject) => {
+    client.api.instruments.upsertInstrumentsProperties(
         buildUpsertInstrumentPropertiesRequest(
-          key,
-          property,
-          instruments
+            key,
+            property,
+            instruments
         )
-      )
-    .then((res: any) => resolve(res.body))
-    .catch((err: any) => reject(err))
+    )
+        .then((res: any) => resolve(res.body))
+        .catch((err: any) => reject(err))
   })
 }
 
@@ -258,26 +259,27 @@ describe('Load Instrument Master', () => {
       upsertInstrumentsFromFile(instrumentsFile, FileType.Json),
       createProperty(securityCurrencyCode)
     ])
-      .then((res) => {
-        return {
-          property: res[1],
-          instruments: addLusidInstrumentIdsFromFile(instrumentsFile, FileType.Json)
+        .then((res) => {
+          return {
+            property: res[1],
+            instruments: addLusidInstrumentIdsFromFile(instrumentsFile, FileType.Json)
           }
         })
-      .then((response) => {
-        return response.instruments.then((instruments) => {
-          return upsertInstrumentProperties(
-            response.property['key'],
-            'currency',
-            instruments)
+        .then((response) => {
+          return response.instruments.then((instruments) => {
+            return upsertInstrumentProperties(
+                response.property['key'],
+                'currency',
+                instruments)
+          })
         })
-      })
-      .then((res: UpsertInstrumentPropertiesResponse) => {
-        console.log(res)
-        done()
-      })
-      .catch((err) => console.log(err.response.statusCode, err.response.statusMessage))
+        .then((res: UpsertInstrumentPropertiesResponse) => {
+          console.log(res)
+          done()
+        })
+        .catch((err) => console.log(err.response.statusCode, err.response.statusMessage))
   })
 })
 
 export {};
+
