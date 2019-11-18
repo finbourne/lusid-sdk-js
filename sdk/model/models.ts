471938b291c999e222d03c1baed498c4b574180d
Concourse <concourse@finbourne.com>
2019-11-18 13:59:43 +0000

    Update SDK 0.10.872

--- sdk/model/models.ts
+++ sdk/model/models.ts
@@ -75,7 +75,6 @@ export * from './resourceListOfPropertyDefinition';
 export * from './resourceListOfReconciliationBreak';
 export * from './resourceListOfScopeDefinition';
 export * from './scopeDefinition';
-export * from './sideConfigurationData';
 export * from './stream';
 export * from './targetTaxLot';
 export * from './targetTaxLotRequest';
@@ -190,7 +189,6 @@ import { ResourceListOfPropertyDefinition } from './resourceListOfPropertyDefini
 import { ResourceListOfReconciliationBreak } from './resourceListOfReconciliationBreak';
 import { ResourceListOfScopeDefinition } from './resourceListOfScopeDefinition';
 import { ScopeDefinition } from './scopeDefinition';
-import { SideConfigurationData } from './sideConfigurationData';
 import { Stream } from './stream';
 import { TargetTaxLot } from './targetTaxLot';
 import { TargetTaxLotRequest } from './targetTaxLotRequest';
@@ -262,7 +260,9 @@ let enumsMap: {[index: string]: any} = {
         "PropertyDefinition.UnitSchemaEnum": PropertyDefinition.UnitSchemaEnum,
         "PropertyDefinition.DomainEnum": PropertyDefinition.DomainEnum,
         "TransactionConfigurationMovementData.MovementTypesEnum": TransactionConfigurationMovementData.MovementTypesEnum,
+        "TransactionConfigurationMovementData.SideEnum": TransactionConfigurationMovementData.SideEnum,
         "TransactionConfigurationMovementDataRequest.MovementTypesEnum": TransactionConfigurationMovementDataRequest.MovementTypesEnum,
+        "TransactionConfigurationMovementDataRequest.SideEnum": TransactionConfigurationMovementDataRequest.SideEnum,
         "TransactionConfigurationTypeAlias.TransactionRolesEnum": TransactionConfigurationTypeAlias.TransactionRolesEnum,
         "TransactionPrice.TypeEnum": TransactionPrice.TypeEnum,
         "TransactionQueryParameters.QueryModeEnum": TransactionQueryParameters.QueryModeEnum,
@@ -348,7 +348,6 @@ let typeMap: {[index: string]: any} = {
     "ResourceListOfReconciliationBreak": ResourceListOfReconciliationBreak,
     "ResourceListOfScopeDefinition": ResourceListOfScopeDefinition,
     "ScopeDefinition": ScopeDefinition,
-    "SideConfigurationData": SideConfigurationData,
     "Stream": Stream,
     "TargetTaxLot": TargetTaxLot,
     "TargetTaxLotRequest": TargetTaxLotRequest,
