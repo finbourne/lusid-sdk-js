06190be564d2348d287ef56d75391a65e27fc7f5
Concourse <concourse@finbourne.com>
2020-07-20 18:54:43 +0000

    Update SDK 0.11.1680

--- sdk/model/models.ts
+++ sdk/model/models.ts
@@ -35,6 +35,7 @@ export * from './instrumentDefinition';
 export * from './instrumentEconomicDefinition';
 export * from './instrumentIdTypeDescriptor';
 export * from './instrumentIdValue';
+export * from './labelValueSet';
 export * from './link';
 export * from './lusidProblemDetails';
 export * from './lusidValidationProblemDetails';
@@ -154,6 +155,7 @@ import { InstrumentDefinition } from './instrumentDefinition';
 import { InstrumentEconomicDefinition } from './instrumentEconomicDefinition';
 import { InstrumentIdTypeDescriptor } from './instrumentIdTypeDescriptor';
 import { InstrumentIdValue } from './instrumentIdValue';
+import { LabelValueSet } from './labelValueSet';
 import { Link } from './link';
 import { LusidProblemDetails } from './lusidProblemDetails';
 import { LusidValidationProblemDetails } from './lusidValidationProblemDetails';
@@ -318,6 +320,7 @@ let typeMap: {[index: string]: any} = {
     "InstrumentEconomicDefinition": InstrumentEconomicDefinition,
     "InstrumentIdTypeDescriptor": InstrumentIdTypeDescriptor,
     "InstrumentIdValue": InstrumentIdValue,
+    "LabelValueSet": LabelValueSet,
     "Link": Link,
     "LusidProblemDetails": LusidProblemDetails,
     "LusidValidationProblemDetails": LusidValidationProblemDetails,
