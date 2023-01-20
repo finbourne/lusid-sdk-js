b7845de0a6b2ffde6116356a173724c92b5e26a0
Concourse <concourse@finbourne.com>
2023-01-20 09:08:11 +0000

    Update SDK 0.11.5159 [rebase]

--- sdk/model/pagedResourceListOfExecution.ts
+++ sdk/model/pagedResourceListOfExecution.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { Execution } from './execution';
 import { Link } from './link';
 
-/**
-* A paginated list of resource that can be returned from a request.
-*/
 export class PagedResourceListOfExecution {
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
-    /**
-    * The resources to list.
-    */
     'values': Array<Execution>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
 
     static discriminator: string | undefined = undefined;
