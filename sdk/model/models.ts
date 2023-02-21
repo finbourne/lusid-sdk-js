185f9cacb97bc6b24b86b803321ba4513062e838
Concourse <concourse@finbourne.com>
2023-02-21 21:41:36 +0000

    Update SDK 0.11.5261 [rebase]

--- sdk/model/models.ts
+++ sdk/model/models.ts
@@ -113,7 +113,6 @@ export * from './creditSpreadCurveData';
 export * from './creditSpreadCurveDataAllOf';
 export * from './creditSupportAnnex';
 export * from './currencyAndAmount';
-export * from './custodianAccount';
 export * from './customEntityDefinition';
 export * from './customEntityDefinitionRequest';
 export * from './customEntityField';
@@ -646,7 +645,6 @@ import { CreditSpreadCurveData } from './creditSpreadCurveData';
 import { CreditSpreadCurveDataAllOf } from './creditSpreadCurveDataAllOf';
 import { CreditSupportAnnex } from './creditSupportAnnex';
 import { CurrencyAndAmount } from './currencyAndAmount';
-import { CustodianAccount } from './custodianAccount';
 import { CustomEntityDefinition } from './customEntityDefinition';
 import { CustomEntityDefinitionRequest } from './customEntityDefinitionRequest';
 import { CustomEntityField } from './customEntityField';
@@ -1411,7 +1409,6 @@ let typeMap: {[index: string]: any} = {
     "CreditSpreadCurveDataAllOf": CreditSpreadCurveDataAllOf,
     "CreditSupportAnnex": CreditSupportAnnex,
     "CurrencyAndAmount": CurrencyAndAmount,
-    "CustodianAccount": CustodianAccount,
     "CustomEntityDefinition": CustomEntityDefinition,
     "CustomEntityDefinitionRequest": CustomEntityDefinitionRequest,
     "CustomEntityField": CustomEntityField,
