e6e9eed5d120ef35c913d14daf4042210fb46132
Adam Massoud <adam.massoud@finbourne.com>
2023-03-01 14:33:12 +0000

    CTECH-2838: Add auth info from preview sdk

--- sdk/test/tutorials/clientBuilder.ts
+++ sdk/test/tutorials/clientBuilder.ts
@@ -1,4 +1,11 @@
-import { Client, Source } from '../../client/client'
+import { AuthenticationMethod, Client, Source } from '../../client/client'
+
+
+if (process.env["FBN_ACCESS_TOKEN"]) {
+  var authMethod = AuthenticationMethod.PersonalAccessToken
+} else {
+  var authMethod = AuthenticationMethod.RefreshingToken
+};
 
 export var client = new Client(
   [Source.Environment, 'FBN_TOKEN_URL'],
@@ -7,4 +14,6 @@ export var client = new Client(
   [Source.Environment, 'FBN_CLIENT_ID'],
   [Source.Environment, 'FBN_CLIENT_SECRET'],
   [Source.Environment, 'FBN_LUSID_API_URL'],
+  authMethod,
+  [Source.Environment, 'FBN_ACCESS_TOKEN'],
 )
