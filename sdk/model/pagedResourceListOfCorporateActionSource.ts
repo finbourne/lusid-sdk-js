267e6d64e8474ed03ca23f4e7353838dd591348d
Concourse <concourse@finbourne.com>
2023-03-03 22:34:05 +0000

    Update SDK 0.11.5300 [rebase]

--- sdk/model/pagedResourceListOfCorporateActionSource.ts
+++ sdk/model/pagedResourceListOfCorporateActionSource.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { CorporateActionSource } from './corporateActionSource';
 import { Link } from './link';
 
-/**
-* A paginated list of resource that can be returned from a request.
-*/
 export class PagedResourceListOfCorporateActionSource {
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
-    /**
-    * The resources to list.
-    */
     'values': Array<CorporateActionSource>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
 
     static discriminator: string | undefined = undefined;
