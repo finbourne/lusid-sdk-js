ead32f15208e488dc08fe04b342c3fb88197517f
Concourse <concourse@finbourne.com>
2023-04-14 18:34:20 +0000

    Update SDK 1.0.72 [rebase]

--- sdk/model/resourceListOfCalendarDate.ts
+++ sdk/model/resourceListOfCalendarDate.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { CalendarDate } from './calendarDate';
 import { Link } from './link';
 
-/**
-* A collection of resources that can be returned from requests.
-*/
 export class ResourceListOfCalendarDate {
-    /**
-    * The resources to list.
-    */
     'values': Array<CalendarDate>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
 
     static discriminator: string | undefined = undefined;
