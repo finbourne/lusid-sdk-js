c698f7f35501e34146bfa8ce3e7595ff6277d1e7
Concourse <concourse@finbourne.com>
2023-05-18 13:46:34 +0000

    Update SDK 1.0.182 [rebase]

--- sdk/model/resourceListOfPlacement.ts
+++ sdk/model/resourceListOfPlacement.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { Link } from './link';
 import { Placement } from './placement';
 
-/**
-* A collection of resources that can be returned from requests.
-*/
 export class ResourceListOfPlacement {
-    /**
-    * The resources to list.
-    */
     'values': Array<Placement>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
 
     static discriminator: string | undefined = undefined;
