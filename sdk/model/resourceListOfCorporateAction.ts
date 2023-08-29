17e6eee76f73c5206ed902cd5453df04dc1fefb8
Concourse <concourse@finbourne.com>
2023-08-29 09:13:48 +0000

    Update SDK 1.0.461 [rebase]

--- sdk/model/resourceListOfCorporateAction.ts
+++ sdk/model/resourceListOfCorporateAction.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { CorporateAction } from './corporateAction';
 import { Link } from './link';
 
-/**
-* A collection of resources that can be returned from requests.
-*/
 export class ResourceListOfCorporateAction {
-    /**
-    * The resources to list.
-    */
     'values': Array<CorporateAction>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
 
     static discriminator: string | undefined = undefined;
