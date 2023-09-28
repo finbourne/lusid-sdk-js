a111653c4b1572ea95611e1e56b5df9bd93ccf73
Concourse <concourse@finbourne.com>
2023-09-28 08:26:45 +0000

    Update SDK 1.0.551 [rebase]

--- sdk/model/pagedResourceListOfComplianceRunInfoV2.ts
+++ sdk/model/pagedResourceListOfComplianceRunInfoV2.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { ComplianceRunInfoV2 } from './complianceRunInfoV2';
 import { Link } from './link';
 
-/**
-* A paginated list of resource that can be returned from a request.
-*/
 export class PagedResourceListOfComplianceRunInfoV2 {
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
-    /**
-    * The resources to list.
-    */
     'values': Array<ComplianceRunInfoV2>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
 
     static discriminator: string | undefined = undefined;
