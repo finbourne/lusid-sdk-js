9d9c4b4628a8a6e082040fd3a07f18320b6b003d
Concourse <concourse@finbourne.com>
2023-10-04 22:14:18 +0000

    Update SDK 1.0.569 [rebase]

--- sdk/model/resourceListOfBlock.ts
+++ sdk/model/resourceListOfBlock.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { Block } from './block';
 import { Link } from './link';
 
-/**
-* A collection of resources that can be returned from requests.
-*/
 export class ResourceListOfBlock {
-    /**
-    * The resources to list.
-    */
     'values': Array<Block>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
 
     static discriminator: string | undefined = undefined;
