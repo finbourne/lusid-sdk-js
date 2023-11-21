6d73cc7563e5147ed6f4d2fc45798f289d5df236
Concourse <concourse@finbourne.com>
2023-11-21 05:35:07 +0000

    Update SDK 1.0.721 [rebase]

--- sdk/model/resourceListOfParticipation.ts
+++ sdk/model/resourceListOfParticipation.ts
@@ -14,29 +14,11 @@ import { RequestFile } from './models';
 import { Link } from './link';
 import { Participation } from './participation';
 
-/**
-* A collection of resources that can be returned from requests.
-*/
 export class ResourceListOfParticipation {
-    /**
-    * The resources to list.
-    */
     'values': Array<Participation>;
-    /**
-    * The URI of the resource list.
-    */
     'href'?: string | null;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
-    /**
-    * The next page of results.
-    */
     'nextPage'?: string | null;
-    /**
-    * The previous page of results.
-    */
     'previousPage'?: string | null;
 
     static discriminator: string | undefined = undefined;
