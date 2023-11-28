696889c25cac25afc1929080882d3c697c00befd
Concourse <concourse@finbourne.com>
2023-11-28 14:17:12 +0000

    Update SDK 1.0.751 [rebase]

--- sdk/model/deleteInstrumentPropertiesResponse.ts
+++ sdk/model/deleteInstrumentPropertiesResponse.ts
@@ -18,9 +18,6 @@ export class DeleteInstrumentPropertiesResponse {
     * The as-at datetime at which properties were deleted.
     */
     'asAt': Date;
-    /**
-    * Collection of links.
-    */
     'links'?: Array<Link> | null;
 
     static discriminator: string | undefined = undefined;
